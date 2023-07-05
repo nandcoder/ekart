1ae6bf15e10115c0ec65743a1d8eb523357806d3
Nand Raj <nandraj2001@gmail.com>
2023-07-06 01:55:30 +0530

    update footer

--- frontend/src/pages/Footer/Footer.js
+++ frontend/src/pages/Footer/Footer.js
@@ -1,26 +1,27 @@
 import React from 'react'
-import {FiFacebook, AiOutlineHeart, AiOutlineInstagram, IoLogoYoutube} from 'react-icons/all';
-import { Input,Stack } from '@chakra-ui/react'
+import { FiFacebook, AiOutlineHeart, AiOutlineInstagram, IoLogoYoutube, IoLogoGithub } from 'react-icons/all';
+import { Input, Stack } from '@chakra-ui/react'
 import './footercss.css'
 import { Link } from 'react-router-dom';
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
     return (
         <div className="footerCmp">
             <footer>
                 <div className="footerCategorie">
                     <h1>Categories</h1>
                     <ul>
-                        <li><Link to = '/shop/?cg=Women'>Women</Link></li>
-                        <li><Link to = '/shop/?cg=Men'>Men</Link></li>
-                        <li><Link to = '/shop/?cg=Shoes'>Shoes</Link></li>
-                        <li><Link to = '/shop/?cg=Watches'>Watches</Link></li>
+                        <li><Link to='/shop/?cg=Women'>Women</Link></li>
+                        <li><Link to='/shop/?cg=Men'>Men</Link></li>
+                        <li><Link to='/shop/?cg=Shoes'>Shoes</Link></li>
+                        <li><Link to='/shop/?cg=Watches'>Watches</Link></li>
                     </ul>
                 </div>
 
                 <div className="fooHelp">
                     <h1>Help</h1>
                     <ul>
-                        <li>Tracke Order</li>
+                        <li>Track Order</li>
                         <li>Returns</li>
                         <li>Shipping</li>
                         <li>FAQs</li>
@@ -30,15 +31,18 @@ const Footer = () => {
                 <div className="footerGetInTouch">
                     <h1>Get in touch</h1>
                     <ul>
-                        <p>Any questions? Let us know in store at BP 473 Complexe universitaire Al Qods, Oujda 60000 or call us on (+212) 65964665</p>
+                        <p>Any questions? Let us know in store at aur github</p>
+                        <li className="footerIcons">
+                            <a href='https://github.com/nandcoder/ekart'><IoLogoGithub size="25" /></a>
+                        </li>
                         <li className="footerIcons">
                             <FiFacebook size="25" />
                         </li>
-                        <li className="footerIcons">  
+                        <li className="footerIcons">
                             <AiOutlineInstagram size="25" />
                         </li>
                         <li className="footerIcons">
-                            <IoLogoYoutube size="25"/>
+                            <IoLogoYoutube size="25" />
                         </li>
                     </ul>
                 </div>
@@ -48,7 +52,7 @@ const Footer = () => {
                     <ul>
                         <li>
                             <Stack spacing={3}>
-                            <Input variant="flushed" placeholder="email@example.com" size="10" width="200px"/>
+                                <Input variant="flushed" placeholder="email@example.com" size="10" width="200px" />
                             </Stack>
                         </li>
                         <li>
@@ -59,18 +63,16 @@ const Footer = () => {
 
                 <div className="creditsIcons">
                     <ul>
-                        <li><img src="https://i.imgur.com/AHCoUZO.png" className="img1"/></li>
-                        <li><img src="https://i.imgur.com/JZRipBg.png" className="img2" /></li>
+                        <li><img src="https://i.imgur.com/JZRipBg.png" className="img1" /></li>
+                        <li><img src="https://i.imgur.com/AHCoUZO.png" className="img2" /></li>
                         <li><img src="https://i.imgur.com/l8OAGyo.png" className="img3" /></li>
-                        <li><img src="https://i.imgur.com/IDHC2iv.png" className="img4" /></li>
+                        <li><img src="https://img.uxwing.com/wp-content/themes/uxwing/download/brands-social-media/rupay-logo-icon.svg" className="img4" /></li>
                     </ul>
-                    
+
                 </div>
-                
-                <div className="paragraphFooter"><p>Copyright ©2021 All rights reserved | This template is made with ♡ by Developers of ESTO</p>
-                <Link to = '' >Abdessamad bourhjoul</Link>
-                <Link to = ''  >Soufian zaam</Link>
-                <Link to = ''  >Souhail ouabou</Link>
+
+                <div className="paragraphFooter">
+                    <p>Copyright ©{currentYear} All rights reserved | This template is made with ♡ by <a href='https://www.linkedin.com/in/nand-raj/' style={{ fontWeight: 500, color: 'blue' }}>NRJ</a></p>
                 </div>
 
 
